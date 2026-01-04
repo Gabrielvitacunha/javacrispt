2b76ecbd3e495c26ac99db5a3e007022be38050f
Gabrielvitacunha <gabrielvita92@gmail.com>
2026-01-04 12:47:20 -0300

    Improve gender and age detection logic in ex015
    Updated script.js to enhance gender and age detection, including dynamic image selection based on age and gender. Changed CSS selector from div to #res for better targeting. Also removed the id 'res' from modelo.html to avoid conflicts.

--- execicios/ex015/script.js
+++ execicios/ex015/script.js
@@ -3,21 +3,49 @@ function verificar(){
     var ano = data.getFullYear()
     var fano = document.getElementById('txtano')
     var res = document.querySelector('div#res')
-    if (fano.value.length == 0 || Number(fano.value)> ano){
-        window.alert('[ERRO] verifique os dando e tente novamente')
-    } else {
-        var fsex = document.getElementsByClassName('radsex')
+    if(fano.value.length == 0|| Number(fano.value) > ano){
+        window.alert(`[ERRO] verifique os dados e tente novamte!`)
+    }else{
+        var fex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
-        var gênero =''
+        var gênero = ''
         var img = document.createElement('img')
-        img.setAttribute('id','foto')
-        if (fsex[0].checked){
-            gênero ='Homem'
-        } else if(fsex[1].checked){
+        img.setAttribute('id','imagen')
+        if(fex[0].checked){
+            gênero = 'homen'
+            if(idade >=0 && idade <10){
+                //criança
+                img.setAttribute('src','imagens/crinçahomen.jpg')
+            }else if (idade < 21){
+                //jovem
+                 img.setAttribute('src','imagens/adolecentehomen.jpg')
+            }else if (idade < 50){
+                //velho
+                 img.setAttribute('src','imagens/adultohomen.jpg')
+            }
+        }else if(fex[1].checked){
             gênero = 'Mulher'
+              if(idade >=0 && idade <10){
+                //criança
+                 img.setAttribute('src','imagens/crinçamulher.jpg')
+            }else if (idade < 21){
+                //jovem
+                 img.setAttribute('src','imagens/adolecentemulher.jpg')
+            }else if (idade < 50){
+                //velho
+                 img.setAttribute('src','imagens/adultamulher.jpg')
+            }
         }
-        res.style.tex
-        res.innerHTML = `Detectamos ${gênero} com ${idade} anos`
+        res.style.textAlign = 'center';
+        res.innerHTML =`Detectamos ${gênero} com ${idade} ano `;
+        res.appendChild(img)
+        
+        
+    
+      
+
     }
+    
+   
     
 }   
